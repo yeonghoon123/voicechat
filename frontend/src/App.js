ecbb639e5c8a6aab1d21da182f78a5ccd664560e
kyh <haa3163314@naver.com>
2021-03-03 17:06:27 +0900

    finally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from "react"
+import Peer from "simple-peer"
+import io from "socket.io-client"
+
+const socket = io.connect("http://localhost:5000")
 
 function App() {
+  const [me ,setMe] = useState("")
+  const [stream ,setStream] = useState()
+  const [RecevingCall ,setRecevingCall] = useState(false)
+  const [caller ,setCaller] = useState("")
+  const [callerSignal ,setCallerSignal] = useState()
+  const [callAccepted ,setCallAccepted] = useState(false)
+  const [idToCall ,setIdToCall] = useState("")
+  const [callEnded ,setCallEnded] = useState(false)
+  const [name ,setName] = useState("")
+
+  const myVideo = useRef()
+  const userVideo =useRef()
+  const connectionRef = useRef()
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({video: true}).then((stream)=> {
+      setStream(stream)
+      myVideo.current.srcObject = stream
+    })
+
+    socket.on('me', (id) => {
+      setMe(id)
+    })
+
+    socket.on("callUser", (data) => {
+      setRecevingCall(true)
+      setCaller(data.from)
+      setName(data.name)
+      setCallerSignal(data.signal)
+    })
+  }, [])
+
+  const callUser = (id) =>{
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream : stream
+    })
+
+    peer.on("signal",(data) =>{
+      socket.emit("callUser",{
+        userToCall:id,
+        signalData: data,
+        from: me,
+        name : name
+      })
+    })
+
+    peer.on("stream", (stream) =>{
+      userVideo.current.srcObject = stream
+    })
+
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true)
+      peer.signal(signal)
+    })
+
+    connectionRef.current = peer
+
+  }
+
+  const answerCall = () =>{
+    setCallAccepted(true)
+    const peer = new Peer({
+      initiator: false,
+        trickle:false,
+        stream: stream
+    })
+
+    peer.on("signal", (data) =>{
+      socket.emit("answerCall", {signal: data, to: caller})
+    })
+
+    peer.on("stream",(strea) => {
+      userVideo.current.srcObject = stream
+    })
+
+    peer.signal(callerSignal)
+    connectionRef.current = peer
+  }
+
+const leaveCall  = () => {
+  setCallEnded(true)
+  connectionRef.current.destroy()
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Zoomish</h1>
+    <div>
+      <div>
+        <div>{stream && <video playsInline muted ref={myVideo} autoPlay />}</div>
+        <div>{callAccepted && !callEnded ? <video playsInline ref={userVideo} autoPlay /> : null }</div>
+      </div>
     </div>
+    </>
   );
 }
 
