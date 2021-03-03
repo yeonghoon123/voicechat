73736b493a7f9993209e0b39bbdd6ec39b76871e
kyh <haa3163314@naver.com>
2021-03-03 13:38:01 +0900

    ws

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const socket = require("socket.io")
+const io = socket(server)
+
+
+
+
+server.listen(5000, () => console.log("server is runnung on port 5000"))
